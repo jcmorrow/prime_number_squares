09a2512f7aaabc320c2ff8a29b96ab0553f0de2b
Joshua Morrow <josh@Joshuas-MBP.hsd1.ca.comcast.net>
2015-06-28 00:57:59 -0700

    Basic implementation of prime-ness. Brute force checking to verify primeness

---
+++ prime_number.rb
@@ -0,0 +1,29 @@
+class PrimeNumber
+	attr_reader :value
+	def initialize value
+		@value = value
+	end
+
+	def self.prime? number
+		return true if [1,2].include? number 
+		return nil if number == 0
+		(2...number).each do |factor|
+			if(number % factor) == 0
+				return false
+			end
+		end
+		return true
+	end
+
+	def self.find_primes n
+		primes = []
+		i = 1
+		while primes.length < n
+			if self.prime? i
+				primes << i
+			end
+			i = i + 1
+		end
+		return primes
+	end
+end
